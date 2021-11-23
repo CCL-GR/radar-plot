942946e2abee895d688dfa1befe0f30334e34d78
carson <carson.linton@gmail.com>
2021-11-24 07:38:25 +0800

    Modified the calculation of attributes

--- radar-plot.py
+++ radar-plot.py
@@ -12,28 +12,29 @@ if not os.path.exists("images"):
 for line in fileinput.input("/home/carson/Documents/4th_student_scores_transposed.csv"):
     student = line.split(sep=' '),
     student_name = line.split(sep=' ')[0],
-    file_name = student_name[0],
-    student2 = line.split(sep=' ')[1],
-    st2sc = student2[0],
-    student3 = line.split(sep=' ')[2],
-    st3sc = student3[0],
-    student4 = line.split(sep=' ')[3],
-    st4sc = student4[0],
-    student5 = line.split(sep=' ')[4],
-    st5sc = student5[0],
-    student6 = line.split(sep=' ')[5],
-    st6sc = student6[0],
-    student7 = line.split(sep=' ')[6],
-    st7sc = student7[0],
-    student8 = line.split(sep=' ')[7],
-    st8sc = student8[0],
-    student9 = line.split(sep=' ')[8],
-    st9sc = student8[0],
+    student_ART = line.split(sep=' ')[1],
+    student_CHINESE = line.split(sep=' ')[2],
+    student_COMPUTER = line.split(sep=' ')[3],
+    student_ELA = line.split(sep=' ')[4],
+    student_MATH = line.split(sep=' ')[5],
+    student_MUSIC = line.split(sep=' ')[6],
+    student_PE = line.split(sep=' ')[7],
+    student_SCIENCE = line.split(sep=' ')[8],
+    stat_visual_spacial = .4*float(student_ART[0]) + .4*float(student_MATH[0]) + .1*float(student_CHINESE[0]) + .1*float(student_PE[0]),
+    stat_introspective = .4*float(student_CHINESE[0]) + .4*float(student_ELA[0]) + .1*float(student_ART[0]) + .1*float(student_MUSIC[0]),
+    stat_social = .4*float(student_CHINESE[0]) + .3*float(student_ELA[0]) + .2*float(student_PE[0]) + .1*float(student_ART[0]),
+    stat_verbal = .4*float(student_CHINESE[0]) + .4*float(student_ELA[0]) + .2*float(student_SCIENCE[0]),
+    stat_logical = .4*float(student_MATH[0]) + .4*float(student_COMPUTER[0]) + .2*float(student_SCIENCE[0]),
+    stat_musical = .7*float(student_MUSIC[0]) + .3*float(student_PE[0]),
+    stat_body = .8*float(student_PE[0]) + .2*float(student_SCIENCE[0]),
+    stat_nature = .6*float(student_SCIENCE[0]) + .3*float(student_COMPUTER[0]) + .1*float(student_MATH[0]),
+
     print(student_name[0]),
 
+
     fig = go.Figure(data=go.Scatterpolar(
-        r=[st2sc[0], st3sc[0], st4sc[0], st5sc[0], st6sc[0], st7sc[0], st8sc[0], st9sc[0]],
-        theta=['[ART]Visual/Spacial Intelligence','[CHINESE]Intra-personal/Introspective Intelligence','[COMPUTER]Interpersonal/Social Intelligence', '[ELA]Verbal/Linguistic Intelligence', '[MATH]Logical/Mathematical Intelligence', '[MUSIC]Musical Rhythmic Intelligence', '[PE]Bodily/Kinesthetic Intelligence', '[SCIENCE]Naturalist Intelligence'],
+        r=[stat_visual_spacial[0], stat_introspective[0], stat_social[0], stat_verbal[0], stat_logical[0], stat_musical[0], stat_body[0], stat_nature[0]],
+        theta=['视觉一空间智能<br>(Visual/Spatial intelligence)','内省智能<br>(Intra-personal/Introspective intelligence)','人际社交智能<br>(Inter-personal/Social intelligence)', '语言智能<br>(Verbal/Linguistic intelligence)', '数理逻辑智能<br>(Logical/Mathematical intelligence)', '音乐韵律智能<br>(Musical/Rhythmic intelligence)', '身体运动智能<br>(Bodily/Kinesthetic intelligence)', '自然探索智能<br>(Naturalist intelligence)'],
         fill='toself'
     ))
     fig.update_layout(
@@ -50,12 +51,3 @@ for line in fileinput.input("/home/carson/Documents/4th_student_scores_transpose
         showlegend=False
     )
     fig.write_image(student_name[0] + ".png")
-
-    # df = pd.DataFrame(dict(
-    #     r=[st2sc[0], st3sc[0], st4sc[0], st5sc[0], st6sc[0], st7sc[0], st8sc[0], st9sc[0]],
-    #     theta=['[ART]Visual/Spacial Intelligence','[CHINESE]Intra-personal/Introspective Intelligence','[COMPUTER]Interpersonal/Social Intelligence', '[ELA]Verbal/Linguistic Intelligence', '[MATH]Logical/Mathematical Intelligence', '[MUSIC]Musical Rhythmic Intelligence', '[PE]Bodily/Kinesthetic Intelligence', '[SCIENCE]Naturalist Intelligence']))
-    # fig = po.line_polar(df, r='r', theta='theta', line_close=True)
-    # fig.update_traces(fill='toself')
-    # fig.write_image(student_name[0] + ".png")
-    # fig.data = []
-    # fig.layout = {}
