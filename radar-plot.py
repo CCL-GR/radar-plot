fee3d1833b9bd4fe3b879281188fc1c91f1e5ace
carson <Carson.Linton@gmail.com>
2021-12-20 16:49:39 +0800

    set for cname+grade

--- radar-plot.py
+++ radar-plot.py
@@ -11,15 +11,17 @@ if not os.path.exists("images"):
 
 for line in fileinput.input(sys.argv[1]):
     student = line.split(sep=' '),
-    student_name = line.split(sep=' ')[0],
-    student_ART = line.split(sep=' ')[1],
-    student_CHINESE = line.split(sep=' ')[2],
-    student_COMPUTER = line.split(sep=' ')[3],
-    student_ELA = line.split(sep=' ')[4],
-    student_MATH = line.split(sep=' ')[5],
-    student_MUSIC = line.split(sep=' ')[6],
-    student_PE = line.split(sep=' ')[7],
-    student_SCIENCE = line.split(sep=' ')[8],
+    student_cname = line.split(sep=' ')[0],
+    student_name = line.split(sep=' ')[1],
+    student_grade_level = line.split(sep=' ')[2],
+    student_ART = line.split(sep=' ')[3],
+    student_CHINESE = line.split(sep=' ')[4],
+    student_COMPUTER = line.split(sep=' ')[5],
+    student_ELA = line.split(sep=' ')[6],
+    student_MATH = line.split(sep=' ')[7],
+    student_MUSIC = line.split(sep=' ')[8],
+    student_PE = line.split(sep=' ')[9],
+    student_SCIENCE = line.split(sep=' ')[10],
     stat_visual_spacial = .4*float(student_ART[0]) + .4*float(student_MATH[0]) + .1*float(student_CHINESE[0]) + .1*float(student_PE[0]),
     stat_introspective = .4*float(student_CHINESE[0]) + .4*float(student_ELA[0]) + .1*float(student_ART[0]) + .1*float(student_MUSIC[0]),
     stat_social = .4*float(student_CHINESE[0]) + .3*float(student_ELA[0]) + .2*float(student_PE[0]) + .1*float(student_ART[0]),
@@ -34,7 +36,7 @@ for line in fileinput.input(sys.argv[1]):
 
     fig = go.Figure(data=go.Scatterpolar(
         r=[stat_visual_spacial[0], stat_introspective[0], stat_social[0], stat_verbal[0], stat_logical[0], stat_musical[0], stat_body[0], stat_nature[0]],
-        theta=['视觉一空间智能<br>(Visual/Spatial intelligence)','内省智能<br>(Intra-personal/Introspective intelligence)','人际社交智能<br>(Inter-personal/Social intelligence)', '语言智能<br>(Verbal/Linguistic intelligence)', '数理逻辑智能<br>(Logical/Mathematical intelligence)', '音乐韵律智能<br>(Musical/Rhythmic intelligence)', '身体运动智能<br>(Bodily/Kinesthetic intelligence)', '自然探索智能<br>(Naturalist intelligence)'],
+        theta=['视觉一空间智能<br>(Visual/Spatial intelligence)','内省智能<br>(Intra-personal/<br>Introspective intelligence)','人际社交智能<br>(Inter-personal/Social intelligence)', '语言智能<br>(Verbal/Linguistic intelligence)', '数理逻辑智能<br>(Logical/<br>Mathematical intelligence)', '音乐韵律智能<br>(Musical/Rhythmic intelligence)', '身体运动智能<br>(Bodily/Kinesthetic intelligence)', '自然探索智能<br>(Naturalist intelligence)'],
         fill='toself'
     ))
     fig.update_layout(
@@ -50,4 +52,4 @@ for line in fileinput.input(sys.argv[1]):
         ),
         showlegend=False
     )
-    fig.write_image(student_name[0] + ".png")
+    fig.write_image(student_grade_level[0] + "_" + student_cname[0] + "_" + student_name[0] + ".png")
