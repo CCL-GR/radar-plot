05a4b5a92fdb8ffd5a624e0723b46ab7582a3132
carson <Carson.Linton@gmail.com>
2021-12-21 15:32:58 +0800

    moved painting

--- radar-plot.py
+++ radar-plot.py
@@ -10,7 +10,20 @@ if not os.path.exists("images"):
     os.mkdir("images")
 
 
-def make_image(plotter, name, cname, grade_level):
+def make_radar_plot_image(plotter, name, cname, grade_level):
+    plotter.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0,5],
+                tick0=0,
+                nticks=5,
+                categoryorder="array",
+                categoryarray=[0,1,2,3,4,5]
+            ),
+        ),
+        showlegend=False
+    )
     plotter.write_image(f"{grade_level}_{cname}_{name}.png")
 
 for line in fileinput.input(sys.argv[1]):
@@ -34,26 +47,10 @@ for line in fileinput.input(sys.argv[1]):
     stat_musical = .7*float(student_MUSIC[0]) + .3*float(student_PE[0]),
     stat_body = .8*float(student_PE[0]) + .2*float(student_SCIENCE[0]),
     stat_nature = .6*float(student_SCIENCE[0]) + .3*float(student_COMPUTER[0]) + .1*float(student_MATH[0]),
-
-    print(student_name[0]), #Shows which students are printing, good for watching if progress is hanging
-
-
     fig = go.Figure(data=go.Scatterpolar(
         r=[stat_visual_spacial[0], stat_introspective[0], stat_social[0], stat_verbal[0], stat_logical[0], stat_musical[0], stat_body[0], stat_nature[0]],
         theta=['视觉一空间智能<br>(Visual/Spatial intelligence)','内省智能<br>(Intra-personal/<br>Introspective intelligence)','人际社交智能<br>(Inter-personal/Social intelligence)', '语言智能<br>(Verbal/Linguistic intelligence)', '数理逻辑智能<br>(Logical/<br>Mathematical intelligence)', '音乐韵律智能<br>(Musical/Rhythmic intelligence)', '身体运动智能<br>(Bodily/Kinesthetic intelligence)', '自然探索智能<br>(Naturalist intelligence)'],
         fill='toself'
     ))
-    fig.update_layout(
-        polar=dict(
-            radialaxis=dict(
-                visible=True,
-                range=[0,5],
-                tick0=0,
-                nticks=5,
-                categoryorder="array",
-                categoryarray=[0,1,2,3,4,5]
-            ),
-        ),
-        showlegend=False
-    )
-    make_image(fig, student_name[0], student_cname[0], student_grade_level[0])
+    make_radar_plot_image(fig, student_name[0], student_cname[0], student_grade_level[0])
+    print(student_name[0]), #Shows which students are printing, good for watching if progress is hanging
